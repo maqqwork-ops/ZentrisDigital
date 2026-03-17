57b0cbc9f92c887eae0566ec1e8c66dca2ec2e21
Hasan Maq <hasanmaq@Hasans-Mac-mini.local>
2026-03-17 06:29:21 -0500

    website update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
  */
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Services from './pages/Services';
@@ -12,14 +13,15 @@ import Portfolio from './pages/Portfolio';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import LocalSEO from './pages/LocalSEO';
-import ScrollToTop from './components/ScrollToTop';
+
+import ScrollTop from './components/ScrollTop';
 
 export default function App() {
   return (
     <Router>
-      <ScrollToTop />
-      <Routes>
+      <ScrollTop />
 
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="services" element={<Services />} />
@@ -27,7 +29,7 @@ export default function App() {
           <Route path="portfolio" element={<Portfolio />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="local-seo/elgin" element={<LocalSEO />} />
+          <Route path="local-seo" element={<LocalSEO />} />
         </Route>
       </Routes>
     </Router>
